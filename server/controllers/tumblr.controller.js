856975599edda357ae013ca93ef1c02ceb8f78b7
Dinesh Vadivel <dvadivel@visa.com>
2018-12-05 18:07:15 +0800

    fix

--- server/controllers/tumblr.controller.js
+++ server/controllers/tumblr.controller.js
@@ -5,8 +5,6 @@ const userLikesService = require('../services/userLikes');
 const fetcherService = require('../services/urlFetcher');
 const resourceservice = require('../services/resource');
 const _ = require('lodash');
-var cron = require('node-cron');
-var cache = require('memory-cache');
 
 module.exports.getTumblr = function (req, res, next) {
   var before = req.query.before;
@@ -40,26 +38,3 @@ module.exports.getTumblr = function (req, res, next) {
 
 };
 
-
-cron.schedule('5 * * * *', () => {
- console.log("Cron Running")
-  var before = cache.get('before')
-  var query = {}
-  if (before) {
-    query.before = before
-  }
-  query.limit = 20
-  userLikesService.get_likes(query)
-    .then(response => {
-      var urls = fetcherService.fetch(response.liked_posts)
-      var items = resourceservice.upload_resource(urls)
-      if ('_links' in response && 'next' in response._links) {
-        var before = response._links.next.query_params.before
-        cache.put('before', before);
-      }
-      res.send(response)
-    }).catch(e => {
-      res.send(e)
-    })
-  console.log('running a task every minute');
-});
